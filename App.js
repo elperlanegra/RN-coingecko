f135875f08d85777d8ef9b4375d91311a13c0cc5
manuelduarte077 <carlosd99m@gmail.com>
2021-09-12 00:28:11 -0600

    feat: add hooks :art:

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
 
-export default function App() {
+const App = () => {
+  
+  const [coins, setCoins] = useState([])
+
+
+
+  // usamos un hooks para agregar la peticipon
+  const loadData = async () => {
+    const res = await fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    );
+    const data = await res.json();
+    setCoins(data)
+  };
+
+  useEffect(() => {
+    console.log('loading');
+    loadData()
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Hola desde React Native</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

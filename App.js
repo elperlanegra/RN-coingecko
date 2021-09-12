80a613cb4429ec5d259169744bce826c4eb9cd9a
manuelduarte077 <carlosd99m@gmail.com>
2021-09-12 15:25:53 -0600

    ✨ feat: done

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TextInput, StatusBar } from 'react-native';
+import CoinItem from './components/CoinItem';
 
 const App = () => {
-  
-  const [coins, setCoins] = useState([])
-
-
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+  const [refreshing, setRefreshing] = useState(false);
 
   // usamos un hooks para agregar la peticipon
   const loadData = async () => {
@@ -13,19 +13,71 @@ const App = () => {
       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     );
     const data = await res.json();
-    setCoins(data)
+    setCoins(data);
   };
 
   useEffect(() => {
-    console.log('loading');
-    loadData()
+    loadData();
   }, []);
 
   return (
-    <View>
-      <Text>Hola desde React Native</Text>
+    <View style={style.container}>
+      <StatusBar backgroundColor="#141414" />
+      <View style={style.header}>
+        <Text style={style.title}>CoinGecko</Text>
+        <TextInput
+          style={style.searchInput}
+          placeholder="Search a Coins"
+          placeholderTextColor="#858585"
+          onChangeText={(text) => setSearch(text)}
+        />
+      </View>
+
+      <FlatList
+        style={style.list}
+        showsVerticalScrollIndicator={false}
+        data={coins.filter((coin) => coin.name.toLowerCase().includes(search) || coin.symbol.toLowerCase().includes(search))}
+        renderItem={({ item }) => {
+          return <CoinItem coin={item} />;
+        }}
+        refreshing={refreshing}
+        onRefresh={ async () => {
+          setRefreshing(true); 
+          await loadData();
+          setRefreshing(false);
+        }}
+      />
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: '#141414',
+    alignItems: 'center',
+    flex: 1,
+  },
+  title: {
+    color: '#fff',
+    marginTop: 10,
+    fontSize: 20,
+  },
+  list: {
+    width: '90%',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    marginBottom: 10,
+  },
+  searchInput: {
+    color: '#fff',
+    borderBottomColor: '#4657CE',
+    borderBottomWidth: 1,
+    width: '40%',
+    textAlign: 'center',
+  },
+});
 
 export default App;
